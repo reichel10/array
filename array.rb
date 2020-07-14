926cb9e57c697ed8fa88805a04eb478d92ddf363
REIKA <reicherururu10@gmail.com>
2020-07-14 22:02:14 +0900

    インデントを修正

--- array.rb
+++ array.rb
@@ -1,13 +1,11 @@
 # pull request
 languages = ["Ruby","PHP","Java"]
 languages.each do |language|
-    if language == "Ruby"
-        puts "Ruby：puts \"Hello World!\""
-    elsif language == "PHP"
-        puts "PHP：echo \"Hello World!\";"
-    else 
-        puts "Java：System.out.println(\"Hello World!\");"
- end
+  if language == "Ruby"
+    puts "Ruby：puts \"Hello World!\""
+  elsif language == "PHP"
+    puts "PHP：echo \"Hello World!\";"
+  else 
+    puts "Java：System.out.println(\"Hello World!\");"
+  end
 end
-
-
