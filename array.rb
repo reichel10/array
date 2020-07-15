24d997d9c926f517d892f2b8532bd5a1cb97038b
REIKA <reicherururu10@gmail.com>
2020-07-15 09:39:44 +0900

    インデントの修正、case文に変更

--- array.rb
+++ array.rb
@@ -1,11 +1,13 @@
 # pull request
 languages = ["Ruby","PHP","Java"]
 languages.each do |language|
-  if language == "Ruby"
-    puts "Ruby：puts \"Hello World!\""
-  elsif language == "PHP"
-    puts "PHP：echo \"Hello World!\";"
-  else 
-    puts "Java：System.out.println(\"Hello World!\");"
+  case language
+  when 'Ruby' 
+    puts 'Ruby：puts \"Hello World!\"'
+  when 'PHP'
+    puts 'PHP：echo \"Hello World!\";'
+  when 'Java'
+    puts 'Java：System.out.println(\"Hello World!\");'
   end
 end
+
